7bdb604ead2f555879b9ef894d15f408f5ce01e4
Peter Nguyen <truonganh20062001@gmail.com>
2024-01-01 00:55:00 +0700

    Spring MVC - Databinding with DropList, Check box, radio button

--- 07-spring-boot-spring-mvc/03-thymeleafdemo - Data Binding/src/main/java/com/peterndta/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/03-thymeleafdemo - Data Binding/src/main/java/com/peterndta/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.peterndta.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -17,6 +19,14 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
         
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+        // log the input
+        System.out.println("theStudent "+theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
     }
 
 }
