620cb20abc52972e2d726239f4f36bacdbd20cfc
Peter Nguyen <truonganh20062001@gmail.com>
2024-01-01 17:24:28 +0700

    Spring MVC - Validation form, @InitBinder, Number Range, Regex, Integer Required Fields, Custom Error message for input String in Int Fields

---
+++ 06-spring-boot-spring-mvc/05-thymeleafdemo - Validation Form & InitBinder/src/main/java/com/peterndta/sprintdemo/mvc/VallidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.peterndta.sprintdemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VallidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VallidationdemoApplication.class, args);
+	}
+
+}
---
+++ 06-spring-boot-spring-mvc/06-thymeleafdemo - Validation Range Number/src/main/java/com/peterndta/sprintdemo/mvc/CustomerController.java
@@ -0,0 +1,52 @@
+package com.peterndta.sprintdemo.mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+    // add InitBinder để trim whitespace
+    // bỏ whitepace ở đầu và cối
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
+        // Log ra xem message error
+        System.out.println("Binding: " + theBindingResult.toString());
+        System.out.println("\n\n");
+        //--------------------------
+
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        else {
+            return "customer-confirmation";
+        }
+    }
+}
