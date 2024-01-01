84d595ee314d699d9d0a3a5e55b961e64534d357
Peter Nguyen <truonganh20062001@gmail.com>
2024-01-01 21:13:10 +0700

    Spring MVC - Custom Vallidation

---
+++ 06-spring-boot-spring-mvc/07-thymeleafdemo - Custom Annotation Validation/src/test/java/com/peterndta/sprintdemo/mvc/VallidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterndta.sprintdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VallidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
