a4e02dca2d56f7e8e69c4aee45e4dace96c795ad
Nguyen Dang Truong Anh <truonganh20062001@gmail.com>
2024-01-09 17:14:47 +0700

    Spring Security - Logout, Authentication

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/peterndta/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.peterndta.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+//        return "plain-login";
+        return "fancy-login";
+    }
+}
