92e3970845dcd087f900fba437b8e5eced805538
Peter Nguyen <truonganh20062001@gmail.com>
2024-09-22 21:26:30 +0700

    Final JUnit

--- 11-Junit/mockito-demo/src/test/java/com/mockito/mockito_demo/MockitoDemoApplicationTests.java
+++ 11-Junit/mockito-demo/src/test/java/com/mockito/mockito_demo/MockitoDemoApplicationTests.java
@@ -1,13 +1,54 @@
 package com.mockito.mockito_demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class MockitoDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void simpleTest() {
+		List listMock = mock(List.class);
+
+		//listMock.size() => 3
+		when(listMock.size()).thenReturn(3);
+		assertEquals(3, listMock.size());
+	}
+
+	@Test
+	void multipleReturns() {
+		List listMock = mock(List.class);
+
+		when(listMock.size()).thenReturn(1).thenReturn(2);
+		assertEquals(1, listMock.size());
+		assertEquals(2, listMock.size());
+		assertEquals(2, listMock.size());
+		assertEquals(2, listMock.size());
+	}
+
+	@Test
+	void parameter() {
+		List listMock = mock(List.class);
+
+		when(listMock.get(0)).thenReturn("SomeString");
+		assertEquals("SomeString", listMock.get(0));
+		assertEquals(null, listMock.get(1));
+	}
+
+	@Test
+	void parameter_multiple() {
+		List listMock = mock(List.class);
+
+		when(listMock.get(Mockito.anyInt())).thenReturn("SomeString");
+		assertEquals("SomeString", listMock.get(0));
+		assertEquals("SomeString", listMock.get(1));
 	}
 
 }
--- 11-Junit/mockito-demo/src/test/java/com/mockito/mockito_demo/business/BusinessImplMocksTest.java
+++ 11-Junit/mockito-demo/src/test/java/com/mockito/mockito_demo/business/BusinessImplMocksTest.java
@@ -1,12 +1,21 @@
 package com.mockito.mockito_demo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.mockito.Mockito.mock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
+@ExtendWith(MockitoExtension.class)
 class BusinessImplMocksTest {
 
+    @Mock DataService dataServiceMock;
+    @InjectMocks BusinessImpl business;
+
+    // Mockito manual without Annotation
     @Test
     void findTheGreatestFromAllData() {
         DataService dataServiceMock = mock(DataService.class);
@@ -19,12 +28,10 @@ class BusinessImplMocksTest {
         assertEquals(25, result);
     }
 
+    // Mockito Annotation
     @Test
     void findTheGreatestFromAllData_OneValue() {
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {35});
-
-        BusinessImpl business = new BusinessImpl(dataServiceMock);
         int result = business.findTheGreatestFromAllData();
         assertEquals(35, result);
     }
